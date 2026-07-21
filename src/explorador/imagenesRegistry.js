0fdd26530a85d9b5db94c44ab50b2d4b13467160
juditfega-boop <juditfega@gmail.com>
2026-07-21 11:30:50 +0200

    InsertarPreliminarSeccion1y2

--- src/explorador/imagenesRegistry.js
+++ src/explorador/imagenesRegistry.js
@@ -3,6 +3,9 @@ import edificioVistaGeneral from '../assets/kit/explorador/edificio-vista-genera
 import edificioCapitulo1Articulos from '../assets/kit/explorador/edificio-capitulo1-articulos.png';
 import edificioCapitulo3Articulos from '../assets/kit/explorador/capitulo3-articulos.png';
 import edificioCapitulo2Secciones from '../assets/kit/explorador/capitulo2-secciones.png';
+import edificioTituloPreliminar from '../assets/kit/explorador/titulo-preliminar-articulos.png';
+import edificioSeccion1 from '../assets/kit/explorador/capitulo2-seccion1-articulos.png';
+import edificioSeccion2 from '../assets/kit/explorador/capitulo2-seccion2-articulos.png';
 
 const imagenesExplorador = {
   'titulo-1-capitulos': edificioTitulo1Capitulos,
@@ -10,6 +13,9 @@ const imagenesExplorador = {
   'capitulo1-articulos': edificioCapitulo1Articulos,
   'capitulo3-articulos': edificioCapitulo3Articulos,
   'capitulo2-secciones': edificioCapitulo2Secciones,
+  'titulo-preliminar-articulos': edificioTituloPreliminar,
+  'capitulo2-seccion1-articulos': edificioSeccion1,
+  'capitulo2-seccion2-articulos': edificioSeccion2,
 };
 
 export default imagenesExplorador;
